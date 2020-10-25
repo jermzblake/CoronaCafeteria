1fa1761ec9b96ba80c4013802c383e0609660897
Jermaine <jermzblake@gmail.com>
2020-10-24 22:07:06 -0400

    fix cafeterias route

--- routes/cafeterias.js
+++ routes/cafeterias.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-const cafeCtrl = require('../controllers')
+const cafeCtrl = require('../controllers/cafeterias')
 
 /* GET cafeterias page. */
 router.get('/', cafeCtrl.index);
