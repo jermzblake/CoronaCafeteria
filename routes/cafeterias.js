d1c2df18a3c3e906516ebd5ba31e921ba811eab4
Jermaine <jermzblake@gmail.com>
2020-10-29 15:40:41 -0400

    add isAuthenticated middleware

--- routes/cafeterias.js
+++ routes/cafeterias.js
@@ -6,9 +6,15 @@ const cafeCtrl = require('../controllers/cafeterias');
 /* GET cafeterias page. */
 router.get('/', cafeCtrl.index);
 router.get('/:id', cafeCtrl.show);
-router.post('/', cafeCtrl.create);
-router.put('/:id', cafeCtrl.update)
-router.delete('/:id', cafeCtrl.delete);
+router.post('/', isLoggedIn, cafeCtrl.create);
+router.put('/:id', isLoggedIn, cafeCtrl.update)
+router.delete('/:id', isLoggedIn, cafeCtrl.delete);
+
+// Insert this middleware for routes that require a logged in user
+function isLoggedIn(req, res, next) {
+    if ( req.isAuthenticated() ) return next();
+    res.redirect('/auth/google');
+  }
 
 // Google OAuth login route
 router.get('/auth/google', passport.authenticate(
