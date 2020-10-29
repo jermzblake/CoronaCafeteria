96799f6772a58d0f9e43d57e941dd05e4bb72492
Jermaine <jermzblake@gmail.com>
2020-10-29 12:07:50 -0400

    add member model

---
+++ models/member.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const memberSchema = new Schema ({
+    name: String,
+    email: String,
+    avatar: String,
+    googleId: String,
+    cafeteria: [{type: Schema.Types.ObjectId, ref: 'Cafeteria' }],
+    comments: [{type: Schema.Types.ObjectId, ref: 'Cafeteria' }],
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('Member', memberSchema)
