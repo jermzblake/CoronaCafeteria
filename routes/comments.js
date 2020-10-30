e5733527b1da131d1f5e99611045516d2111a0d6
Jermaine <jermzblake@gmail.com>
2020-10-29 21:37:51 -0400

    restrict comments to logged in users. Implement dynamic UI for CRUD on cafeterias and menus. Must be owner of cafeteria to see

--- routes/comments.js
+++ routes/comments.js
@@ -3,7 +3,13 @@ var router = express.Router();
 const commentsCtrl = require('../controllers/comments');
 
 /* comments routes */
-router.post('/cafeterias/:id/comments', commentsCtrl.create)
+router.post('/cafeterias/:id/comments', isLoggedIn, commentsCtrl.create)
 router.delete('/comments/:id', commentsCtrl.delete)
+
+// Insert this middleware for routes that require a logged in user
+function isLoggedIn(req, res, next) {
+    if ( req.isAuthenticated() ) return next();
+    res.redirect('/auth/google');
+    }
 
 module.exports = router;
